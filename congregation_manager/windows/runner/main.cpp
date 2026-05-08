0f925ddf2c04c40b2b5d0f173778975df0d13f88
jeremy-phoenix <jeyearwood@phoenixsoftgy.com>
2026-05-08 14:26:49 -0400

    Add CenterWindowOnMonitor function to center the application window

--- congregation_manager/windows/runner/main.cpp
+++ congregation_manager/windows/runner/main.cpp
@@ -5,6 +5,35 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+namespace {
+
+void CenterWindowOnMonitor(HWND window) {
+  RECT window_rect;
+  if (!::GetWindowRect(window, &window_rect)) {
+    return;
+  }
+
+  HMONITOR monitor = ::MonitorFromWindow(window, MONITOR_DEFAULTTOPRIMARY);
+  MONITORINFO monitor_info{};
+  monitor_info.cbSize = sizeof(monitor_info);
+  if (!::GetMonitorInfo(monitor, &monitor_info)) {
+    return;
+  }
+
+  const RECT work_area = monitor_info.rcWork;
+  const int window_width = window_rect.right - window_rect.left;
+  const int window_height = window_rect.bottom - window_rect.top;
+  const int work_area_width = work_area.right - work_area.left;
+  const int work_area_height = work_area.bottom - work_area.top;
+  const int origin_x = work_area.left + (work_area_width - window_width) / 2;
+  const int origin_y = work_area.top + (work_area_height - window_height) / 2;
+
+  ::SetWindowPos(window, nullptr, origin_x, origin_y, 0, 0,
+                 SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
+}
+
+}  // namespace
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
@@ -30,6 +59,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   if (!window.Create(L"congregation_manager", origin, size)) {
     return EXIT_FAILURE;
   }
+  CenterWindowOnMonitor(window.GetHandle());
   window.SetQuitOnClose(true);
 
   ::MSG msg;
